3750c30a82fd8c37019cc5dd7b74f4cfd330e812
Armina/Mina <145488343+MinaWebDev@users.noreply.github.com>
2023-11-21 19:07:22 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+/* Created by Armina Reyes */
+function toggle(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+}
